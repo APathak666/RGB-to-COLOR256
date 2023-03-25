f98cfa2bd2784406be77e0930556933d3f6c6c3b
Arya Pathak <percapitaoutcome@gmail.com>
2023-03-25 18:33:07 +0530

    Update helper.h

--- helper.h
+++ helper.h
@@ -10,7 +10,7 @@ int red, green, blue;
 unsigned char* image_rgb_to_color256(unsigned int dim_x, unsigned int dim_y, unsigned char* image_buf);
 unsigned char* image_color256(unsigned int dim_x, unsigned int dim_y);
 void test_func();
-void print_arr(unsigned char* buf, int size);
+void print_arr(unsigned char* buf, int start_idx, int end_idx);
 void print_list();
 void error_handler(int error, char* str);
 void unpack(int compact);
