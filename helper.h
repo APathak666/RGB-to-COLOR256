81b45e05bacbfe4961cfa5e8c190985caf152ede
Arya Pathak <percapitaoutcome@gmail.com>
2023-03-27 23:17:34 +0530

    Improved coding style

--- helper.h
+++ helper.h
@@ -15,4 +15,14 @@ void print_list();
 void error_handler(int error, char* str);
 void unpack(int compact);
 
+//error codes and messages
+enum err_code {MALLOC_ERR = 1, PALETTE_ERR, INSERT_ERR, DIM_ERR, BUFF_ERR, INP_ERR};
+
+#define MALLOC_MSG  "allocating memory"
+#define PALETTE_MSG "color palette size exceeded"
+#define INSERT_MSG  "value already exists in table"
+#define DIM_MSG     "incorrect dimensions"
+#define BUFF_MSG    "color256 buffer returned NULL"
+#define INP_MSG     "NULL input"
+
 #endif
